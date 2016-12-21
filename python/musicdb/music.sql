d7f4ee2964f1eb1156b5c09fe592b44e4c3e4fc8
Damian O'Brien <damianob@gmail.com>
2016-12-21 15:59:31 -0600

    increased widths of names

--- python/musicdb/music.sql
+++ python/musicdb/music.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `album`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `album` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
+  `name` text NOT NULL,
   `artist_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS `artist`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `artist` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
+  `name` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `track`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `track` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
+  `name` text NOT NULL,
   `album_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
