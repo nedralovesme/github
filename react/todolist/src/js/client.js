52b7a19d5393549c280a48fa445ce1c86e903086
Damian O'Brien <damianob@gmail.com>
2017-02-12 22:19:30 -0600

    React Starter projects

---
+++ react/todolist/src/js/client.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import TodoApp from "./components/TodoApp";
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<TodoApp />, app);
